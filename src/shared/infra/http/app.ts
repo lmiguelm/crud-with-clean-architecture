3a587a81de44912bd38e82e5a0fe604969e8af19
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-03-20 20:48:27 -0300

    adicionando routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,12 @@
 import cors from 'cors';
 
+import { routes } from './routes';
+
 import express from 'express';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 export { app };
